7aec619470456c041e6a626d5cd02cdf4da5203a
Hiroshi MITANI <hmitani0429@gmail.com>
2017-12-14 18:04:23 +0900

    不要なlodashを削除

--- template/src/index.js
+++ template/src/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash'
 import numRef from './ref.json'
 
 /**
@@ -7,9 +6,8 @@ import numRef from './ref.json'
  * @example webpackNumbers.numToWord(1) // => 'One'
  */
 export const numToWord = num => {
-  return _.reduce(numRef, (accum, ref) => {
-    return ref.num === num ? ref.word : accum
-  }, '')
+  let pair = numRef.find(ref => ref.num === num)
+  return pair ? pair.word : ''
 }
 
 /**
@@ -18,7 +16,6 @@ export const numToWord = num => {
  * @example webpackNumbers.wordToNum('One') // => 1
  */
 export const wordToNum = word => {
-  return _.reduce(numRef, (accum, ref) => {
-    return ref.word === word && word.toLowerCase() ? ref.num : accum
-  }, -1)
+  let pair = numRef.find(ref => ref.word === word)
+  return pair ? pair.num : -1
 }
