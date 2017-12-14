78906dbb576f7246494d523c35cf4785c37a9406
Hiroshi MITANI <hmitani0429@gmail.com>
2017-12-14 17:44:36 +0900

    キャメルケースの関数名を修正

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '{{ name }}.js',
-    library: '{{camelcase name}}',
+    library: '{{kebab_to_camel name}}',
     libraryTarget: 'umd'
   },
   module: {
